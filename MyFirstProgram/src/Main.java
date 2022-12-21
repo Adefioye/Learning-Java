e13cbbb650e4634831fc3474c265643751b5a4d0
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-12-21 13:07:39 -0600

    Learnt basic syntax on print statements, reading inputs and writing basic expressions

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		int a = 5;
+//		double pi = 3.14;
+//		boolean isValid = true;
+//		char symbol = '@';
+//		String name = "Koko";
+		
+//		System.out.println("I love pizza");
+//		System.out.println("I love eating!");
+		
+		
+		// Swap two variables
+//		String x = "Kool-aid";
+//		String y = "Water";
+//		String temp = x;
+//		
+//		x = y;
+//		y = temp;
+//		
+//		System.out.println(x);
+//		System.out.println(y);
+		
+		// Accepting user inputs
+		
+	}
+
+}
---
+++ ReadingInput/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Reading input from the console
+		
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		System.out.print("What is your name? : ");
+//		String name = scanner.nextLine();
+//		
+//		System.out.print("What is your age? :");
+//		int age = scanner.nextInt();
+//		
+//		scanner.nextLine();
+//		System.out.print("What is your favorite food? :");
+//		String food = scanner.nextLine();
+//		
+//		
+//		System.out.println("My name is "+name );
+//		System.out.println("I am "+ age + " year's old");
+//		System.out.println("My favorite food is "+ food);
+		
+		// Arithmetic expressions
+		
+		// expression = operators and operands
+		// operators = +, -, /, * 
+		// operands = values, variables, numbers, quantity
+		
+//		int friends = 10;
+//		
+//		friends--;
+		
+		// To get integer division not truncating values, use double type
+//		int friends = 10;
+//		friends = friends / 3;
+		
+		double friends = 10;
+		friends = friends / 3;
+		
+		System.out.println(friends);
+		
+		
+	}
+
+}
