355020f67ecc741a980a306c62a5907908d8f477
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-12-23 12:51:39 -0600

    Learnt string methods

---
+++ Strings/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String name = "SalaManDer";
+		
+//		boolean result = name.equalsIgnoreCase("salamander");
+//		int result = name.length();		
+//		char result = name.charAt(5);
+//		int result =  name.indexOf("m");
+//		String result = name.toUpperCase();
+//		String result = name.toLowerCase();
+//		String result = name.trim();		
+//		String result = name.replace("D", "M");
+		boolean result = name.isEmpty();
+		
+		System.out.println(result);
+	}
+
+}
